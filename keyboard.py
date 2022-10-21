5222c04a30fa83fa6ecb223b871aba2baf376bf5
Arseniy <115782526+Gegtr1@users.noreply.github.com>
2022-10-21 11:23:41 +0300

    Keyboard script

---
+++ keyboard.py
@@ -0,0 +1,15 @@
+llist = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", " ", "\n", ",", "."]
+i = 0
+for i in range(i, i+1):
+    y = int(input("Код символа\n"))
+    print (llist[y])
+    y1 = int(input("Код символа\n"))
+    print (llist[y1])
+    y2 = int(input("Код символа\n"))
+    print (llist[y2])
+    y3 = int(input("Код символа\n"))
+    print (llist[y3])
+    y4 = int(input("Код символа\n"))
+    print (llist[y4])
+    print(llist[y],llist[y1],llist[y2],llist[y3],llist[y4])
+
